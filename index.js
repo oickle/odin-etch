de2fdaaea9ac64e1f065824e66a48f5eefbf998e
oickle <oycell@aol.com>
2023-08-02 16:35:27 -0400

    Add drawing functionality

--- index.js
+++ index.js
@@ -1,17 +1,36 @@
 let canvas = document.querySelector('.canvas');
 
+let mouseDown = false;
+
+// Sets whether the mouse is being held down or not
+function getMouseStatus() {
+    document.body.onmousedown = () => {
+        mouseDown = true;
+    }
+    document.body.onmouseup = () => {
+        mouseDown = false;
+    };
+}
+
 function fillRow(size){
     // Creates a row
-    for(let i = 0; i < size; i++) {
+    for (let i = 0; i < size; i++) {
         const newCell = document.createElement('div');
         newCell.classList.add('cell');
+
+        // Instantly changes color of clicked cell
+        newCell.addEventListener('mousedown', paint);
+        
+        // Checks whether mouse is held down when entering to allow painting by mouse dragging
+        newCell.addEventListener('mouseover', checkPaintDrag);
+        
         canvas.appendChild(newCell);
     }
 };
 
 function fillGrid(size) {
     // Creates equal amount of rows to columns
-    for(let i = 0; i < size; i++) {
+    for (let i = 0; i < size; i++) {
         fillRow(size);
     }
 
@@ -19,4 +38,18 @@ function fillGrid(size) {
     canvas.style['max-width'] = `${size * 25}px`;
 }
 
+// Called when mouse enters cell, allows for mouse to be dragged to paint
+function checkPaintDrag(e) {
+    if (mouseDown === true) { // checks whether mouse is being held down
+        paint(e);
+    }
+}
+
+// Changes selected cell's color
+function paint(e) {
+    const cell = e.target;
+    cell.style['background-color'] = 'black';
+}
+
+getMouseStatus();
 fillGrid(16);
