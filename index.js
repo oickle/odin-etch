23be5e60f124629c7a32ab631d22610a8850cb49
oickle <oycell@aol.com>
2023-08-03 20:06:42 -0400

    Complete basic project functionality

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 let canvas = document.querySelector('.canvas');
+let changeSizeButton = document.querySelector('.size-button');
 
 let mouseDown = false;
+let hoverMode = false;
 
 // Sets whether the mouse is being held down or not
 function getMouseStatus() {
@@ -16,6 +18,12 @@ function fillRow(size){
     // Creates a row
     for (let i = 0; i < size; i++) {
         const newCell = document.createElement('div');
+        
+        // Cell sizes change to fit the 400px by 400px grid, rather than the grid itself changing
+        let scale = 400 / size // Calculates the maximum size for the cells
+        console.log(scale);
+        newCell.style['width'] = `${scale}px`;
+        newCell.style['height'] = `${scale}px`;
         newCell.classList.add('cell');
 
         // Instantly changes color of clicked cell
@@ -28,19 +36,25 @@ function fillRow(size){
     }
 };
 
-function fillGrid(size) {
-    // Creates equal amount of rows to columns
+// Creates an equal amount of rows to columns
+function fillCanvas(size) {
     for (let i = 0; i < size; i++) {
         fillRow(size);
     }
+}
 
-    // Sets the maximum width of the entire canvas to the exact size of all the cells
-    canvas.style['max-width'] = `${size * 25}px`;
+// Deletes all old cells on the grid
+function clearCanvas(){
+    var cell = canvas.firstElementChild;
+    while (cell) {
+        canvas.removeChild(cell);
+        cell = canvas.firstElementChild;
+    }
 }
 
 // Called when mouse enters cell, allows for mouse to be dragged to paint
 function checkPaintDrag(e) {
-    if (mouseDown === true) { // checks whether mouse is being held down
+    if (mouseDown === true || hoverMode) { // checks whether mouse is being held down
         paint(e);
     }
 }
@@ -51,5 +65,21 @@ function paint(e) {
     cell.style['background-color'] = 'black';
 }
 
+// Called from html 'onclick' function
+function changeCanvasSize() {
+    let newSize = +prompt("What size do you want? (MAX is 100)");
+    newSize = newSize > 0 ? newSize : 1; // prevents sizes of 0 or negative values
+    newSize = newSize < 100 ? newSize : 100; // limits max grid size to 100
+    
+    clearCanvas(); // deletes every child div
+    fillCanvas(newSize);
+}
+
+// Toggles hover mode when checkbox is clicked
+function toggleHover() {
+    hoverMode = !hoverMode;
+}
+
 getMouseStatus();
-fillGrid(16);
+fillCanvas(16);
+
